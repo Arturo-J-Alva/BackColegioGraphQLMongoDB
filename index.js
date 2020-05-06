f67d956588966d7982ebe8af2b60309c201330d6
Arturo-J-Alva <arturo.j.a.m@outlook.com>
2020-05-05 20:14:00 -0500

    copie codigo pero era de apollo koa que me confunde y probare otro codigo mejor

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./db/schema')
 const resolvers = require('./db/resolvers')
 
+//Declarando 2 librerías de node y 2 externas y carpeta contenedora para upload
+const { createWriteStream, unlink } = require('fs');
+const mkdirp = require('mkdirp');
+const shortid = require('shortid');
+const UPLOAD_DIR = './uploads';
+
 const conectarDB = require('./config/db')
 
 const jwt = require('jsonwebtoken')
@@ -9,10 +15,58 @@ require('dotenv').config({ path: 'variables.env' })
 
 conectarDB()
 
+// Declarando carpeta upload
+mkdirp.sync(UPLOAD_DIR);
+
+const storeUpload = async (upload) => {
+    const { createReadStream, filename, mimetype } = await upload;
+    const stream = createReadStream();
+    const id = shortid.generate();
+    const path = `${UPLOAD_DIR}/${id}-${filename}`;
+    const file = { id, filename, mimetype, path };
+  
+    // Almacene el archivo en el sistema de archivos.
+    await new Promise((resolve, reject) => {
+      // Crea una secuencia en la que se escribirá la carga.
+      const writeStream = createWriteStream(path);
+  
+      //Cuando la carga esté completamente escrita, resuelva la promesa.
+      writeStream.on('finish', resolve);
+  
+      // Si hay un error al escribir el archivo, elimine el archivo parcialmente escrito y rechace la promesa.
+      writeStream.on('error', (error) => {
+        unlink(path, () => {
+          reject(error);
+        });
+      });
+  
+      // En el nodo <= 13, los errores no se propagan automáticamente entre flujos canalizados. 
+      // Si hay un error al recibir la carga, destruya la secuencia de escritura con el error correspondiente.
+      stream.on('error', (error) => writeStream.destroy(error));
+  
+      //Canalice la carga en la secuencia de escritura.
+      stream.pipe(writeStream);
+    });
+  
+    // Grabe los metadatos del archivo en la base de datos.
+    db.get('uploads').push(file).write();
+  
+    return file;
+  };
+
+
 //servidor
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    uploads: {
+        // Los límites aquí deberían ser más estrictos que la configuración para la 
+        // infraestructura circundante, como Nginx, de modo que los errores se 
+        // puedan manejar con elegancia mediante graphql-upload:
+        // https://github.com/jaydenseric/graphql-upload#type-processrequestoptions
+        maxFileSize: 10000000, // 10 MB
+        maxFiles: 20,
+      },
     context: ({req}) => {
         //console.log(req.headers['content-length'])
         
@@ -35,7 +89,8 @@ const server = new ApolloServer({
                
             }
         }
-    }
+    },
+    storeUpload
 })
 
 //arrancar el servidor
